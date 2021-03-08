91b4cc2ca1b930f2c67d91195127c1abc1e3699a
willkrakow <willkrakow@gmail.com>
2021-03-07 19:05:03 -0500

    Cloudinary picture and text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import Styled from "styled-components";
 import LocationOn from "@material-ui/icons/LocationOn";
 import Add from "@material-ui/icons/Add";
+import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
+import { Image } from "cloudinary-react";
+
+const BigHeader = Styled.h1`
+color: rgba(253,187,45,0.8);
+font-weight: 200;
+font-size: 4rem;
+letter-spacing: 0.9;
+`;
+
 const Button = Styled.button`
 padding: 12px 16px;
 border: none;
@@ -10,34 +20,36 @@ display: block;
 margin: auto;
 margin-bottom: 1em;
 background: transparent;
-color: rgba(240, 250, 255, 0.6);
+color: #edf1f7;
+background: rgba(34,193,195, 0.8);
+background: linear-gradient(70deg, rgba(34,193,195,0.8) 0%, rgba(253,187,45,0.8) 100%);
 font-weight: 600;
 cursor: pointer;
 font-size: 18px;
 border-radius: 8px;
 transition: all 0.3s ease;
 &:hover {
-  background: rgba(240, 250, 255, 0.6);
-  color: #343c46;
+  transform: scale(1.1);
+  background: linear-gradient(70deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
 }
 `;
 
 const PlainText = Styled.p`
 font-size: 18px;
-color: rgba(0, 15, 30, 0.8);
-font-weight: 200;
+color: rgba(10, 15, 30, 0.8);
+font-weight: 600;
 `;
 
 const SmallText = Styled(PlainText)`
-font-size: 14px;
-color: rgba(0, 15, 30, 0.6);
-font-weight: 400;
+font-size: 18px;
+color: #22c1c3;
+font-weight: 300;
 `;
 
 const LocationIcon = Styled(LocationOn)`
-display: block;
-width: 100%;
-margin: auto;
+color: rgba(34, 193, 195, 0.8);
+font-size: 4rem;
+vertical-align: bottom;
 `;
 
 const TextArea = Styled.textarea.attrs((props) => ({
@@ -52,6 +64,16 @@ border-radius: 16px;
 font-family: 'Arial', sans-serif;
 padding: 16px;
 
+`;
+
+const FlexWrapper = Styled.div`
+display: flex;
+justify-content: center;
+min-width: 50%;
+`;
+
+const FlexChild = Styled.div`
+flex-basis: 400px;
 `;
 
 export default function App() {
@@ -91,7 +113,45 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Mango</h1>
+      <BigHeader>Mango</BigHeader>
+      <Image
+        cloudName="djmk8xgrk"
+        publicId="mango_fz0jtf.jpg"
+        width="400"
+        crop="scale"
+      />
+      <FlexWrapper>
+        <FlexChild>
+          <h2 style={{ textAlign: "left" }}>Hi hooman.</h2>
+          <p style={{ textAlign: "left" }}>Hope you're doing alright today.</p>
+          <p style={{ textAlign: "left" }}>
+            My name's Mango, and I live in Hillsborough, NC on Margaret Lane. I
+            live to hunt bugs and bully my brother (he's a doggo) and play
+            frogger in the street.
+          </p>
+          <p style={{ textAlign: "left" }}>
+            If you're reading this, you probably found me doing one of the
+            things above.
+          </p>
+          <h3 style={{ textAlign: "left" }}>What to do with me</h3>
+          <ul style={{ textAlign: "left" }}>
+            <li>
+              Give me snuggles. (Don't worry, I got shot with rabbies. No...
+              wait.. okay it was something like that.
+            </li>
+            <li>
+              Let my parents know where I am. Their names are Will and Laura and
+              they are LAME but nice enough.
+            </li>
+            <li>More snuggles pls.</li>
+          </ul>
+          <h3 style={{ textAlign: "left" }}>Why the frick am I out here</h3>
+          <p style={{ textAlign: "left" }}>
+            Because I like being outside, duh. Sunshine is good for doggos and
+            ferocious Mangos and hoomans alike.
+          </p>
+        </FlexChild>
+      </FlexWrapper>
       <form
         action=""
         method="POST"
@@ -100,11 +160,12 @@ export default function App() {
         form-name="location-data"
       >
         <input type="hidden" name="location" value={location} />
-        <LocationIcon
-          style={{ color: "rgba(240, 250, 255, 0.6)", fontSize: "2rem" }}
-        />
         <SmallText>Your location</SmallText>
-        <PlainText>{location.toLocaleString()}</PlainText>
+        <PlainText>
+          {" "}
+          <LocationIcon />
+          {location.toLocaleString()}
+        </PlainText>
         {!message && (
           <Button type="button" onClick={handleMessage}>
             <Add style={{ verticalAlign: "bottom" }} /> Add message
